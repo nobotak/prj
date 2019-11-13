8765bf7d517e77affb0332407e0c3b8eec134498
nobotak <nobotak.com@gmail.com>
2019-11-13 10:56:34 +0100

    dodana funkcja shout() / zmiana kolejnosci wywolania funkcji

--- gittie.py
+++ gittie.py
@@ -1,19 +1,22 @@
 def introduce():
     print ("Hello I am Gittie Smitty!!")
 
-introduce()
-
 
 def addnumbers ():
     num_1 = int(input("wpisz numer 1:"))
     num_2 = int(input("wpisz numer 2:"))
     print(num_1 + num_2)
 
-addnumbers()    
 
 def joke():
     print ("My dog has no nose. -So how does he smell? -Awful!")
 
-joke()
 
-print ("hwdp")
+
+def shout():
+    print ("hwdp")
+
+introduce()
+addnumbers() 
+joke()  
+shout()    
