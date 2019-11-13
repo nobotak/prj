d490ecf8442274a13c7353ccfe97126587f6ac3b
nobotak <nobotak.com@gmail.com>
2019-11-13 10:43:50 +0100

    funkcja dodawania 2 numerow

--- gittie.py
+++ gittie.py
@@ -2,3 +2,10 @@ def introduce():
     print ("Hello I am Gittie!!")
 
 introduce()
+
+def addnumbers ():
+    num_1 = int(input("wpisz numer 1:"))
+    num_2 = int(input("wpisz numer 2:"))
+    print(num_1 + num_2)
+
+addnumbers()    
